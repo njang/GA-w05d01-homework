9a5aa4477e1ee855a79abec0b97926f6a9f67170
njang <neo.w.jang@gmail.com>
2018-01-09 10:25:16 -0600

    Add comments and CORS to server.js

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/kittens', (req, res) => res.send('Hello Kittens!'))
+
+
+
+// Handle Cross-origin request (CORS)
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function(req, res, next) {
+  // Handle the get for this route
+});
+
+app.post('/', function(req, res, next) {
+ // Handle the post for this route
+});
+
+// Listening on port 3000. Node will display the following message if the server is running.
 app.listen(3000, ()=>{
     console.log("I am listening");
 });
